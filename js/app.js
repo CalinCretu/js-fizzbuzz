75ec68eccf7c67586c2ef78259067140d0ace240
CalinCretu <calincretu1412@gmail.com>
2023-10-06 14:42:55 +0200

    ADD - print_3&5_mulpitles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('prova')
+console.log('numeri da 1 a 100')
+for (let i = 0; i < 100; i++) {
+    const n = i + 1
+    console.log(n)
+}
+
+console.log('numeri da 1 a 100 multipli di 3')
+for (let i = 0; i < 100; i++) {
+    const n = i + 1
+    const resto = n % 3
+
+    if (resto === 0) {
+        console.log(n)
+    }
+}
+
+console.log('numeri da 1 a 100 multipli di 3')
+for (let i = 0; i < 100; i++) {
+    const n = i + 1
+    const resto = n % 5
+
+    if (resto === 0) {
+        console.log(n)
+    }
+}
