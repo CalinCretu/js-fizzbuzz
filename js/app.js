f4694d1e782b57246ca92c20c8d921c3facd7c7a
CalinCretu <calincretu1412@gmail.com>
2023-10-06 16:50:49 +0200

    ADD - js_corrections_and_css

--- js/app.js
+++ js/app.js
@@ -1,18 +1,33 @@
-console.log('numeri da 1 a 100')
+const gridDOMElement = document.querySelector('.grid')
 
-for (let i = 0; i < 100; i++) {
-    const n = i + 1
 
-    if (n % 3 != 0 && n % 5 != 0) {
-        console.log(n)
-    }
-    else if (n % 3 === 0 && n % 5 === 0) {
+
+for (let i = 1; i <= 100; i++) {
+    const n = i
+    const boxDOMElement = document.createElement('div')
+    boxDOMElement.classList.add('box')
+
+    if (n % 3 === 0 && n % 5 === 0) {
+        boxDOMElement.classList.add('bkpink')
+        boxDOMElement.innerHTML = 'FizzBuzz'
         console.log('FizzBuzz')
     }
     else if (n % 3 === 0) {
+        boxDOMElement.classList.add('bkgreen')
+        boxDOMElement.innerHTML = 'Fizz'
         console.log('Fizz')
     }
     else if (n % 5 === 0) {
+        boxDOMElement.classList.add('bkyellow')
+        boxDOMElement.innerHTML = 'Buzz'
         console.log('Buzz')
     }
+    else {
+        boxDOMElement.classList.add('bkblue')
+        boxDOMElement.innerHTML = n
+        console.log(n)
+    }
+    gridDOMElement.append(boxDOMElement)
 }
+
+
