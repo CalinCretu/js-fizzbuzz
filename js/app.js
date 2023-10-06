97f98877e5bf640c6c73481965e13e99585fc284
CalinCretu <calincretu1412@gmail.com>
2023-10-06 15:31:01 +0200

    ADD - fizz_buzz_&_fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,25 +1,18 @@
 console.log('numeri da 1 a 100')
+
 for (let i = 0; i < 100; i++) {
     const n = i + 1
-    console.log(n)
-}
 
-console.log('numeri da 1 a 100 multipli di 3')
-for (let i = 0; i < 100; i++) {
-    const n = i + 1
-    const resto = n % 3
-
-    if (resto === 0) {
+    if (n % 3 != 0 && n % 5 != 0) {
         console.log(n)
     }
-}
-
-console.log('numeri da 1 a 100 multipli di 5')
-for (let i = 0; i < 100; i++) {
-    const n = i + 1
-    const resto = n % 5
-
-    if (resto === 0) {
-        console.log(n)
+    else if (n % 3 === 0 && n % 5 === 0) {
+        console.log('FizzBuzz')
+    }
+    else if (n % 3 === 0) {
+        console.log('Fizz')
+    }
+    else if (n % 5 === 0) {
+        console.log('Buzz')
     }
 }
